1b93b61bb43f022ffea55a7b15e39d66d9710216
Samuel Dela Cruz <samsamcoding@gmail.com>
2021-11-25 15:58:56 +0800

    #2 Improve handing try and catch thru asyncMiddleWare

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,24 +1,17 @@
 const Task = require('../models/Task')
-const getAllTasks = async (req, res ) => {
-    try {
+const asyncWrapper = require('../middleware/async')
+
+const getAllTasks = asyncWrapper( async (req, res ) => {
         const tasks = await Task.find({})
         res.status(200).json({ tasks })
-    } catch (error) {
-        res.status(500).json({ msg: error })
-    }
-}
+})
 
-const createTask = async (req, res) => {
-    try {
+const createTask = asyncWrapper( async (req, res) => {
         const task = await Task.create(req.body)
         res.status(201).json({ task })
-    } catch (error) {
-        res.status(500).json({ msg: error })
-    }
-}
+})
 
-const getTask = async (req, res) => {
-    try {
+const getTask = asyncWrapper( async (req, res) => {
         const {id:taskId} = req.params
         const task = await Task.findOne({ _id: taskId })
 
@@ -26,13 +19,9 @@ const getTask = async (req, res) => {
             return res.status(404).json({ msg: `No task with id: ${taskId}`})
         }
         res.status(200).json({ task })
-    } catch (error) {
-        res.status(500).json({ msg: error })
-    }
-}
+})
 
-const deleteTask = async (req, res) => {
-    try {
+const deleteTask = asyncWrapper( async (req, res) => {
         const  {id: taskId} = req.params;
         const task = await Task.findOneAndDelete({ _id: taskId })
 
@@ -40,15 +29,10 @@ const deleteTask = async (req, res) => {
             return res.status(404).json({ msg: `No task with id: ${taskId}`})
         }
     res.status(200).json({ task })
-    } catch (error) {
-        res.status(500).json({ msg: error })
-    }
-}
+})
 
-const updateTask = async (req, res) => {
-    try {
+const updateTask = asyncWrapper( async (req, res) => {
         const {id: taskId} = req.params;
-        
         // the option object will automatically update the response and will implement validation
         const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, { new: true, runValidators: true
         })
@@ -56,10 +40,7 @@ const updateTask = async (req, res) => {
         return res.status(404).json({ msg: `No task with id: ${taskId}`})
     }  
         res.status(200).json({ task })
-    } catch (error) {
-        res.status(500).json({ msg: error })
-    }
-}
+})
 
 module.exports = {
     getAllTasks, 
