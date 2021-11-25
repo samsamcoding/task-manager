f6fef276d404246f8cfd0db00633d9db55b37369
Samuel Dela Cruz <samsamcoding@gmail.com>
2021-11-25 15:31:50 +0800

    #1 Initial Commit

---
+++ controllers/tasks.js
@@ -0,0 +1,70 @@
+const Task = require('../models/Task')
+const getAllTasks = async (req, res ) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+
+const getTask = async (req, res) => {
+    try {
+        const {id:taskId} = req.params
+        const task = await Task.findOne({ _id: taskId })
+
+        if(!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskId}`})
+        }
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+
+const deleteTask = async (req, res) => {
+    try {
+        const  {id: taskId} = req.params;
+        const task = await Task.findOneAndDelete({ _id: taskId })
+
+        if(!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskId}`})
+        }
+    res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+
+const updateTask = async (req, res) => {
+    try {
+        const {id: taskId} = req.params;
+        
+        // the option object will automatically update the response and will implement validation
+        const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, { new: true, runValidators: true
+        })
+    if(!task) {
+        return res.status(404).json({ msg: `No task with id: ${taskId}`})
+    }  
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+
+module.exports = {
+    getAllTasks, 
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+} 
